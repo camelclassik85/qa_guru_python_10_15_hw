c3c9c6438437c37d52e64dfd58492c00cfe37f41
Alexander_Dulalaev <camel1985@mail.ru>
2024-03-15 00:49:40 +0300

    add test_parametrize.py

--- homework/test_parametrize.py
+++ homework/test_parametrize.py
@@ -2,16 +2,17 @@
 Переопределите параметр с помощью indirect параметризации на уровне теста
 """
 import pytest
+from selene import browser, command
 
 
-@pytest.fixture()
-def browser():
-    pass
+@pytest.mark.parametrize('desktop_browser', [(1600, 900), (1280, 1000)], indirect=True)
+def test_github_desktop(desktop_browser):
+    browser.open('/')
+    browser.element('.HeaderMenu-link--sign-in').click()
 
 
-def test_github_desktop(browser):
-    pass
-
-
-def test_github_mobile(browser):
-    pass
+@pytest.mark.parametrize('mobile_browser', [(880, 700), (640, 480)], indirect=True)
+def test_github_mobile(mobile_browser):
+    browser.open('/')
+    browser.element('.Button--link').click()
+    browser.element('a[class*="HeaderMenu-link--sign-in"]').perform(command.js.scroll_into_view).click()
