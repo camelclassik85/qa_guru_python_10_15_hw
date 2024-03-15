eddaf67b174d90201cfde97342a89cf236a9c824
Alexander_Dulalaev <camel1985@mail.ru>
2024-03-15 13:04:15 +0300

    add test_skip.py

--- homework/conftest.py
+++ homework/conftest.py
@@ -4,11 +4,13 @@ from selene import browser
 
 FULL_HD = (1920, 1080)
 HD = (1080, 720)
+DESCTOP = [FULL_HD, HD]
 MOB1 = (800, 480)
 MOB2 = (480, 320)
+MOB = [MOB1, MOB2]
 
 
-@pytest.fixture(params=[FULL_HD, HD])
+@pytest.fixture(params=DESCTOP)
 def desktop_browser(request):
     width, height = request.param
     browser.config.window_width = width
@@ -20,7 +22,7 @@ def desktop_browser(request):
     browser.quit()
 
 
-@pytest.fixture(params=[MOB1, MOB2])
+@pytest.fixture(params=MOB)
 def mobile_browser(request):
     width, height = request.param
     browser.config.window_width = width
@@ -30,3 +32,16 @@ def mobile_browser(request):
     yield browser
 
     browser.quit()
+
+
+@pytest.fixture(params=DESCTOP + MOB)
+def common_browser(request):
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+    browser.config.base_url = 'https://github.com'
+
+    yield browser
+
+    browser.quit()
+
--- homework/test_skip.py
+++ homework/test_skip.py
@@ -1,18 +1,26 @@
 """
-Параметризуйте фикстуру несколькими вариантами размеров окна
+Параметризуйте фикстуру несколькими вариантами размеров окна.
 Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
 """
 import pytest
+from homework.conftest import DESCTOP, MOB
+from selene import browser, command
 
 
-@pytest.fixture()
-def browser():
-    pass
+def test_github_desktop(common_browser):
+    screen = (common_browser.config.window_width, common_browser.config.window_height)
+    if screen in DESCTOP:
+        browser.open('/')
+        browser.element('.HeaderMenu-link--sign-in').click()
+    else:
+        pytest.skip(reason=f'размеры экрана {screen} не десктопные({DESCTOP})')
 
 
-def test_github_desktop(browser):
-    pass
-
-
-def test_github_mobile(browser):
-    pass
+def test_github_mobile(common_browser):
+    screen = (common_browser.config.window_width, common_browser.config.window_height)
+    if screen in MOB:
+        browser.open('/')
+        browser.element('.Button--link').click()
+        browser.element('a[class*="HeaderMenu-link--sign-in"]').perform(command.js.scroll_into_view).click()
+    else:
+        pytest.skip(reason=f'размеры экрана {screen} не мобильные({MOB})')
